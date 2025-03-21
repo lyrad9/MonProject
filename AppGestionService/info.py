d78bbd54449c0bab0c8939d9c4a0c3c76c182d41
Essame Adam <edjabeadam1@gmail.com>
2025-03-21 09:21:07 +0100

    Add files via upload

---
+++ AppGestionService/info.py
@@ -0,0 +1,9 @@
+
+
+EMAIL_USE_TLS = True # ativation de la securite TLS
+EMAIL_HOST = 'smtp.gmail.com' # serveur SMTP de mon fournisseur : GMAIL
+EMAIL_HOST_USER = 'edjabeadam1@gmail.com' # serveur SMTP de mon fournisseur : GMAIL
+EMAIL_HOST_PASSWORD = 'babr uqxq ztgf laif' #mot de passe pour application
+EMAIL_PORT = 587 # port securise pour l'envoi d'email
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER #
+
